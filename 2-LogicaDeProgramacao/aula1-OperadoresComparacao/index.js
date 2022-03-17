899f130c867f1aa23493a29835fd58fc078d4878
tfdlucas <lalisergia171@gmail.com>
2022-03-17 14:45:24 -0300

    master

---
+++ 2-LogicaDeProgramacao/aula1-OperadoresComparacao/index.js
@@ -0,0 +1,18 @@
+/*
+OPERADORES DE COMPARAÇÃO
+
+> Maior que
+>= Maior ou igual a
+< Menor que
+<= Menor ou igual a
+== Igualdade (valor) Não recomendado
+=== Igualdade Estrita (valor e tipo)
+!= Diferente (valor) Não recomendado
+!== Diferente Estrito (valor e tipo)
+
+*/
+
+console.log(10 > 5);
+console.log(10 >= 15);
+console.log(10 !== '10');
+console.log(10 === '10');
