1e6a7e3c689fc18f752f3abf0a1a1f22d4630530
tfdlucas <lalisergia171@gmail.com>
2022-03-19 02:32:34 -0300

    'master'

--- 2-LogicaDeProgramacao/aula5-TabelaIMC/assets/js/main.js
+++ 2-LogicaDeProgramacao/aula5-TabelaIMC/assets/js/main.js
@@ -1,32 +1,70 @@
-function meuEscopo () { // Função Principal
-    const peso = peso.querySelector('.input-peso');
-    const altura = altura.querySelector('.input-altura'); 
-    const resultado = document.querySelector('.resultado');
+const form = document.querySelector('.form'); // Captura do formulário
 
-
-    function recebeEventoForm (evento) {
-        evento.preventDefault(); // Não envia o formulário
+    form.addEventListener('submit', function (e) { // Adicionou o evento de 'submit' no formulário
+        e.preventDefault();  // Para travar o envio do formulário
         
-        peso = Number(peso);
-        altura = Number(altura);
-        const imc = peso / (altura * altura);
+        // e.target mostra qual elemento foi clicado na página
+        const inputPeso = e.target.querySelector('.input-peso'); 
+        const inputAltura = e.target.querySelector('.input-altura');
 
-        if (imc < 18.5) {
-            resultado.innerHTML = `<p>Seu IMC é ${imc} (Abaixo do peso)</p>`;
-        } else if (imc >= 18.5 && imc <= 24.9) {
-            resultado.innerHTML = `<p>Seu IMC é ${imc} (Peso normal)</p>`;
-        } else if (imc >= 25 && imc <= 29.9) {
-            resultado.innerHTML = `<p>Seu IMC é ${imc} (Sobrepeso)</p>`;
-        } else if (imc >= 30 && imc <= 34.9) {
-            resultado.innerHTML = `<p>Seu IMC é ${imc} (Obesidade grau 1)</p>`;
-        } else if (imc >= 35 && imc <= 39.9) {
-            resultado.innerHTML = `<p>Seu IMC é ${imc} (Obesidade grau 2)</p>`;
-        } else if (imc >= 40) {
-            resultado.innerHTML = `<p>Seu IMC é ${imc} (Obesidade grau 3)</p>`;
+        const peso = Number(inputPeso.value); // Converte o input para Number
+        const altura = Number(inputAltura.value); // Converte o input para Number
+
+        if (!peso) {
+            setResultado(`Peso inválido.`, false) // False retorna para o isValid p ter fundo red
+            return // Se o peso for diferente de number, vai enviar a mensagem e parar a funcao
         }
+
+        if (!altura) {
+            setResultado(`Altura inválida.`, false) // False retorna para o isValid p ter fundo red
+            return // Se a altura for diferente de number, vai enviar a mensagem e parar a funcao
+        }
+
+        const imc = getImc(peso, altura); // Const é a funcao getImc que recebe peso e altura
+        const nivelImc = getNivelImc(imc); // Recebe a função getNivelImc que contém o array 
+
+        const msg = `Seu IMC é ${imc} (${nivelImc}).`; // Define msg
+
+        setResultado(msg, true); // Chama a função setResultado
+    });
+
+    function getNivelImc (imc) { // Recebe o valor do imc
+        const nivel = ['Abaixo do peso', // 0
+                        'Peso normal', // 1
+                        'Sobrepeso',  // 2
+                        'Obesidade grau 1', // 3
+                        'Obesidade grau 2', // 4
+                        'Obesidade grau 3'] // 5
+
+        if (imc >= 39.9) return nivel[5];
+        if (imc >= 34.9) return nivel[4];
+        if (imc >= 29.9) return nivel[3];
+        if (imc >= 24.9) return nivel[2];
+        if (imc >= 18.5) return nivel[1];
+        if (imc < 18.5)  return nivel[0];
     }
 
-    form.addEventListener('submit', recebeEventoForm);
-}
+    function getImc (peso, altura) { // Recebe peso e altura e faz o cálculo do IMC
+        const imc  = peso / altura ** 2;
+        return imc.toFixed(2); // Retorna o IMC com 2 casas decimais
+    }
 
-meuEscopo(); // Chamando Função Principal
+    function criaParagrafo () {
+        const p = document.createElement('p'); // Criação de um paragrafo
+        return p;
+    }
+
+    function setResultado (msg, isValid) { // Verifica se o resultado é true ou false
+        const resultado = document.querySelector('.resultado');
+        resultado.innerHTML = ''; // O que for enviado no (msg) vem parar aqui no html do resultado
+        const p = criaParagrafo();
+
+        if (isValid) {
+            p.classList.add('verde'); // Cria classe para estilizar no css (Verde)
+        } else {
+            p.classList.add('vermelho'); // Cria classe para estilizar no css (Vermelho)
+        }
+
+        p.innerHTML = msg; // Exibe a msg = p
+        resultado.appendChild(p); // Adiciona o paragrafo dentro do resultado 
+    }
